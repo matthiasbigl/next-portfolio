4d34d67df4c1bd8409a71c45eba43039fa5a88cb
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-05-13 10:30:21 +0200

    inital Push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
-import {Layout} from "../Components";
+import {Layout} from "../components";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
