7ea0e8774b915b7cbf6646a73dd3eab2f9c52db2
matthiasbigl <73336850+matthiasbigl@users.noreply.github.com>
2022-05-13 10:06:58 +0200

    inital Push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 import {Layout} from "../Components";
 
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import Typewriter from "typewriter-effect";
-import {useState} from "react";
+import React, {useState} from "react";
 import Image from "next/image";
 import profile from "../assets/images/matthiasbigl.jpg";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+
+import Link from "next/link";
 
 
 export default function Home() {
+    library.add(fab);
     const [isTyping, setIsTyping] = useState(true);
     // @ts-ignore
     return (
@@ -14,15 +20,13 @@ export default function Home() {
                 <title>Matthias Bigl Webportfolio</title>
             </Head>
             <div
-                className={'bg-gradient-to-br from-cyan-500 to-blue-700 h-[60vh] w-3/4 p-8 pb-12 m-8 rounded-lg text-center flex justify-center items-center'}>
-
-                <div>
+                className={'bg-gradient-to-br from-cyan-500 to-blue-700 w-3/4 p-8 pb-12 m-8 rounded-lg text-center flex justify-center items-center'}>
 
 
-
-                        {
-                            isTyping ?
-                                <h1 className={'text-white font-bold text-5xl align-middle'}>
+                {
+                    isTyping ?
+                        <div className={'h-[60vh] flex justify-center items-center'}>
+                            <h1 className={'text-white font-bold text-5xl align-middle'}>
                                 <Typewriter
 
 
@@ -51,29 +55,112 @@ export default function Home() {
 
 
                                 />
-                                </h1>:
-                                <div className='grid-cols-2 grid place-content-center'>
-                                   <div
-                                   className={'col-span-2 flex w-full justify-center items-center'}>
-                                     <Image
-                                     src={profile}
-                                     width={200}
-                                     height={200}
-                                     className={'rounded-full'}
-                                     alt={'Matthias Bigl'}
-                                     />
-                                   </div>
+                            </h1>
+                        </div>
+                        :
+                        <div className='grid-cols-2 grid place-content-center w-full'>
+                            <div
+                                className={'col-span-2 flex justify-center items-center mb-5'}>
+                                <Image
+                                    src={profile}
+                                    width={200}
+                                    height={200}
+                                    className={'rounded-full'}
+                                    alt={'Matthias Bigl'}
+                                />
 
+                            </div>
+                            <div
+                                className={'col-span-2 flex justify-center items-center mb-2'}>
+
+
+                                <div className="flex flex-row mt-1" role="group">
+
+                                    <a
+                                        href="https://www.instagram.com/matthias_bigl"
+                                        target="_blank"
+                                        className="group flex px-2 py-2 font-medium text-white hover:text-gray-900 transition ease-in-out duration-500"
+                                    >
+
+                                        <FontAwesomeIcon
+                                            size="2x"
+                                            icon={["fab", "instagram"]}
+                                        />
+                                    </a>
+                                    <a
+                                        href="https://www.linkedin.com"
+                                        target="_blank"
+                                        className="group flex px-2 py-2 font-medium text-white hover:text-gray-900 transition ease-in-out duration-500"
+                                    >
+                                        <FontAwesomeIcon
+                                            size="2x"
+                                            icon={["fab", "linkedin"]}
+                                        />
+                                    </a>
+                                    <a
+                                        href="https://www.github.com/matthiasbigl"
+                                        target="_blank"
+                                        className="group flex px-2 py-2 font-medium text-white hover:text-gray-900 transition ease-in-out duration-500"
+                                    >
+                                        <FontAwesomeIcon
+                                            size="2x"
+                                            icon={["fab", "github"]}
+                                        />
+                                    </a>
 
                                 </div>
-                        }
+                            </div>
+                            <div
+                                className={'col-span-2 flex justify-center items-center pb-8 mb-5'}>
+                                <h1 className={'text-white font-bold text-5xl align-middle'}>
+                                    Matthias Bigl
+                                </h1>
+                            </div>
+                            <div className={'col-span-2 lg:col-span-1 text-white flex justify-center mb-8'}>
+                                <div className={'align-middle w-4/5'}>
+                                    <h1 className={'text-2xl font-semibold'}>
+                                        About me:
+                                    </h1>
+                                    <p className={'text-lg'}>
+                                        I am a software engineer with a passion for learning and creating.
+                                        I am currently studying at <a
+                                        href="https://www.htl-hl.ac.at/"
+                                        target="_blank"
+                                        className="font-semibold "
+                                    >
+                                        HTL-Hollabrunn
+                                    </a>
+                                        I hope to become a full stack developer in the near future.
+                                        Hopefully I can impress you with my skills and projects.
+                                    </p>
+                                </div>
 
 
+                            </div>
+                            <div className={'col-span-2 lg:col-span-1 text-white flex justify-center mb-8'}>
+                                <div className={'align-middle w-4/5'}>
+                                    <h1 className={'text-2xl font-semibold'}>
+                                        My skills:
+                                    </h1>
+                                    <p className={'text-lg'}>
+                                        I mostly got my education in Java, PHP and SQL.
+                                        However I am a self-taught developer in JavaScript, React Node.js and Next.js.
+                                        I am also familiar with Python as you will see in my projects.
+                                        I am constantly learning new technologies and frameworks.
+                                    </p>
+                                </div>
 
 
-                </div>
+                            </div>
+
+
+                        </div>
+                }
+
 
             </div>
+
+
         </div>
 
     )
